542d1d63eff85d05d3abfaefe8f59b15dbf3440d
PiotrRomanczuk <p.romanczuk@gmail.com>
2023-08-21 10:36:18 +0200

    .

--- server/config/winstonLogger.js
+++ server/config/winstonLogger.js
@@ -4,17 +4,18 @@ const loggerConfig = {
 	level: 'info',
 	format: winston.format.combine(
 		winston.format.timestamp(),
+		winston.format.colorize(),
 		winston.format.json(),
 		winston.format.prettyPrint()
 	),
 	defaultMeta: { service: 'user-service' },
 	transports: [
-		//
-		// - Write all logs with importance level of `error` or less to `error.log`
-		// - Write all logs with importance level of `info` or less to `combined.log`
-		//
-		new winston.transports.File({ filename: 'error.log', level: 'error' }),
-		new winston.transports.File({ filename: 'combined.log' }),
+		new winston.transports.File({
+			filename: './server/logs/error.log',
+			level: 'error',
+		}),
+		new winston.transports.File({ filename: './server/logs/combined.log' }),
+		new winston.transports.Console(),
 	],
 };
 
