19b75e4cdbf57dfc7b75d4dd0d45ae1ea097252e
PiotrRomanczuk <77811265+PiotrRomanczuk@users.noreply.github.com>
2023-08-29 13:12:45 +0200

    Setting up the SQLite DB

---
+++ server/database/sqlite.js
@@ -0,0 +1,23 @@
+const sqlite3 = require('sqlite3').verbose();
+
+const startDB = (database) => {
+
+    const db = new sqlite3.Database(database)
+
+	const sql_query_create_table = `CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
+    first_name VARCHAR(255) NOT NULL, 
+    last_name VAARCHAR(255) NOT NULL, 
+    email VARCHAR(255) NOT NULL, 
+    password VARCHAR(255))`;
+
+    db.
+
+	// new sqlite3.Database(database, sqlite3.OPEN_READWRITE, (err) => {
+	// 	if (err) return console.error(err.message);
+	// 	else return console.log('Create Table successfully');
+    // });
+
+};
+
+module.exports = startDB;
