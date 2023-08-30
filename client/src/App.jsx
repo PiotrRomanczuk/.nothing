5ac9bfdabe9a99c145620375e690d495e9c7f907
PiotrRomanczuk <77811265+PiotrRomanczuk@users.noreply.github.com>
2023-08-30 13:31:53 +0200

    feat(frontEnd): Implementing the basic redux reducer - auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,20 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 
 import SignIn from './components/pages/SignIn';
-import Dashboard from './components/dashboard/Dashboard';
+import Dashboard from './components/pages/Dashboard';
 
 function App() {
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
+
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path='/' element={<SignIn />} />
 				<Route path='/dashboard' element={<Dashboard />} />
+				{/* <Route path='/profile' element={<Profile />} /> // TODO make those components
+				<Route path='/settings' element={<Settings />} /> */}
 			</Routes>
 		</BrowserRouter>
 	);
