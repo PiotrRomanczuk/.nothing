3cae4c2b1ef91199001f6fa10d2edf19bcdacf8e
PiotrRomanczuk <77811265+PiotrRomanczuk@users.noreply.github.com>
2023-08-30 18:48:43 +0200

    Added Chat component

--- client/src/components/shared/Card.jsx
+++ client/src/components/shared/Card.jsx
@@ -2,21 +2,21 @@ import React from 'react';
 
 const Card = ({ title, content }) => {
 	return (
-		<div class='max-w-md bg-white rounded-xl overflow-hidden shadow-lg m-2 flex'>
+		<div className='max-w-md bg-white rounded-xl overflow-hidden shadow-lg m-2 flex'>
 			{/* <div class='w-1/3'>
 				<img class='w-full' src='image.jpg' alt='Card Image' />
 			</div> */}
-			<div class=' px-6 py-4'>
-				<div class='font-bold text-xl mb-2'>{title}</div>
-				<p class='text-gray-700 text-base'>{content}</p>
-				<div class='mt-4 flex'>
-					<span class='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
+			<div className=' px-6 py-4'>
+				<div className='font-bold text-xl mb-2'>{title}</div>
+				<p className='text-gray-700 text-base'>{content}</p>
+				<div className='mt-4 flex'>
+					<span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
 						Tag1
 					</span>
-					<span class='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
+					<span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
 						Tag2
 					</span>
-					<span class='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700'>
+					<span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700'>
 						Tag3
 					</span>
 				</div>
