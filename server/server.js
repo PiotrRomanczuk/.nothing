647439165d91c4bb2919bb989bcc8fb2cf4e46a7
PiotrRomanczuk <77811265+PiotrRomanczuk@users.noreply.github.com>
2023-09-16 15:53:07 +0200

    feat:Adding basic cypress testinf for the front end

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 
 const cors = require('cors');
@@ -16,14 +17,11 @@ const authRouter = require('./routes/authRoutes');
 
 const PORT = 8080;
 
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: '30mb', extended: true }));
 app.use(cors(corsOptions));
 app.use(expressWinston.logger(winstonLoggerConfig));
-app.use((err, req, res, next) => {
-	console.error(err.stack);
-	res.status(500).send(`Error: ${err.stack}`);
-});
 
 app.use('/notes/', notesRouter);
 app.use('/user/', authRouter);
