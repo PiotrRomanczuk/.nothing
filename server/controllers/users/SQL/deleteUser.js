ca4b1bb2dcebf38fca21c1ac6e77abef18ce0478
PiotrRomanczuk <77811265+PiotrRomanczuk@users.noreply.github.com>
2023-09-17 18:21:17 +0200

    Added the delete and update user controllers for SQLite

---
+++ server/controllers/users/SQL/deleteUser.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const router = express.Router();
+
+const deleteUser = async (req, res) => {
+	try {
+		const userId = req.params.userId;
+
+		const db = new sqlite3.Database(pathDB, sqlite3.OPEN_READWRITE, (err) => {
+			if (err) {
+				console.error(err.message);
+				return res.status(500).json({ error: 'Database error' });
+			}
+		});
+
+		db.serialize(() => {
+			const deleteUserQuery = 'DELETE FROM users WHERE id = ?';
+
+			db.run(deleteUserQuery, [userId], (err) => {
+				if (err) {
+					console.error(`SQL delete error: ${err.message}`);
+					return res.status(500).json({ error: 'Unable to delete user.' });
+				}
+				res.status(204).end(); // No content response indicates success
+			});
+		});
+	} catch (err) {
+		console.error(err);
+		return res.status(500).json({ error: 'Internal server error' });
+	}
+};
+
+module.exports = deleteUser;
--- server/tests/utils/email&passValidate.test.js
+++ server/tests/utils/email&passValidate.test.js
@@ -1,46 +1,46 @@
-// const {
-// 	validateEmail,
-// 	validatePassword,
-// } = require('../../utils/email&passValidate');
-// const chai = require('chai');
-// const expect = chai.expect;
+const {
+	validateEmail,
+	validatePassword,
+} = require('../../utils/email&passValidate');
+const chai = require('chai');
+const expect = chai.expect;
 
-// describe('Email Validation', () => {
-// 	it('should return true for a valid email', () => {
-// 		expect(validateEmail('test@example.com')).to.be.true;
-// 	});
+describe('Email Validation', () => {
+	it('should return true for a valid email', () => {
+		expect(validateEmail('test@example.com')).to.be.true;
+	});
 
-// 	it('should return false for an invalid email', () => {
-// 		expect(validateEmail('invalid-email')).to.be.false;
-// 	});
+	it('should return false for an invalid email', () => {
+		expect(validateEmail('invalid-email')).to.be.false;
+	});
 
-// 	it('should return false for an email with spaces', () => {
-// 		expect(validateEmail('test with spaces@example.com')).to.be.false;
-// 	});
-// });
+	it('should return false for an email with spaces', () => {
+		expect(validateEmail('test with spaces@example.com')).to.be.false;
+	});
+});
 
-// describe('Password Validation', () => {
-// 	it('should return true for a valid password', () => {
-// 		expect(validatePassword('Passw0rd!')).to.be.true;
-// 	});
+describe('Password Validation', () => {
+	it('should return true for a valid password', () => {
+		expect(validatePassword('Passw0rd!')).to.be.true;
+	});
 
-// 	it('should return false for a password with insufficient length', () => {
-// 		expect(validatePassword('Short1!')).to.be.false;
-// 	});
+	it('should return false for a password with insufficient length', () => {
+		expect(validatePassword('Short1!')).to.be.false;
+	});
 
-// 	it('should return false for a password without an uppercase letter', () => {
-// 		expect(validatePassword('password1!')).to.be.false;
-// 	});
+	it('should return false for a password without an uppercase letter', () => {
+		expect(validatePassword('password1!')).to.be.false;
+	});
 
-// 	it('should return false for a password without a lowercase letter', () => {
-// 		expect(validatePassword('PASSWORD1!')).to.be.false;
-// 	});
+	it('should return false for a password without a lowercase letter', () => {
+		expect(validatePassword('PASSWORD1!')).to.be.false;
+	});
 
-// 	it('should return false for a password without a digit', () => {
-// 		expect(validatePassword('Password!')).to.be.false;
-// 	});
+	it('should return false for a password without a digit', () => {
+		expect(validatePassword('Password!')).to.be.false;
+	});
 
-// 	it('should return false for a password without a special character', () => {
-// 		expect(validatePassword('Password1')).to.be.false;
-// 	});
-// });
+	it('should return false for a password without a special character', () => {
+		expect(validatePassword('Password1')).to.be.false;
+	});
+});
